c34aeeb5f392f8d36bdee5f4acfd96ff288c9ac8
Unknown <oliver.li47@gmail.com>
2018-07-27 19:57:04 +1200

    Added basic query handling

---
+++ 306Project/src/main/java/main/Query.java
@@ -0,0 +1,36 @@
+package main;
+
+/**
+ * Created by olive on 27/07/2018.
+ */
+public class Query {
+
+    /**
+     * What to do if no option, or option 3 is selected
+     * @param fileName
+     * @param processorNumber
+     */
+    public static void handle(String fileName, int processorNumber) {
+        //Code
+    }
+
+    /**
+     * Handles the case of -p is selected. This is the parallel computing option
+     * @param fileName
+     * @param processorNumber
+     * @param coreNumber
+     */
+    public static void handle(String fileName, int processorNumber, int coreNumber) {
+        //Code
+    }
+
+    /**
+     * Handles the case if -o is selected. This is the OUTPUT option
+     * @param fileName
+     * @param processorNumber
+     * @param outputName
+     */
+    public static void handle(String fileName, int processorNumber, String outputName) {
+        //Code
+    }
+}
