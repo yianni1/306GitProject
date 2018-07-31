d670b5a85f3e7358cdf22f9efb4b61d3f0964630
Ray <raymondyoung122@gmail.com>
2018-07-31 14:39:51 +1200

    Refactoring of methods from TaskGraph into Schedule.

--- 306Project/src/main/java/graph/TaskGraph.java
+++ 306Project/src/main/java/graph/TaskGraph.java
@@ -1,9 +1,6 @@
 package graph;
 
-import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.HashSet;
-import java.util.List;
 
 /**
  * Created by olive on 29/07/2018.
@@ -12,14 +9,14 @@ public class TaskGraph {
 
     private HashSet<TaskEdge> edges; //The edges of the graph
     private HashSet<TaskNode> nodes;
-    private List<TaskNode> availableNodes;
-    
+    private HashSet<TaskNode> schedulable;
+
     public TaskGraph() {
-    	edges = new HashSet<TaskEdge>();
-    	nodes = new HashSet<TaskNode>();
-    	availableNodes = new ArrayList<TaskNode>();
+        edges = new HashSet<TaskEdge>();
+        nodes = new HashSet<TaskNode>();
+        schedulable = new HashSet<TaskNode>();
     }
-    
+
     /**
      * Adds an edge
      * @param edge
@@ -27,46 +24,33 @@ public class TaskGraph {
     public void addEdge(TaskEdge edge) {
         this.edges.add(edge);
     }
-    
+
     /**
      * Adds a node
      * @param node
      */
     public void addNode(TaskNode node) {
-    	 this.nodes.add(node);
-    	 node.setGraph(this); //Nodes maintain a refernce to the graph
+        this.nodes.add(node);
     }
-    
-    public HashSet<TaskNode> getNodes() {
-    	return nodes;
-    }
-    
+
+    public HashSet<TaskNode> getNodes() { return this.nodes; }
+
     public HashSet<TaskEdge> getEdges() {
-    	return this.edges;
+        return this.edges;
     }
 
-    /**
-     * Indicates that a node has been made available
-     * @param node the node that is now avialable
-     */
-    void nodeMadeAvailable(TaskNode node) {
-        availableNodes.add(node);
+
+    public void updateSchedulableNodes(TaskNode added) {
+        this.schedulable.remove(added);
+
+        // Send a message to all the nodes chlidren.
+        TaskNode childNode;
+        for (TaskEdge e: added.getOutgoingEdges()) {
+            childNode = e.getEndNode();
+            // Add one to the childNodes 'parentsvisited' field.
+        }
+
+
     }
 
-    /**
-     * Indicates that a node is no longer available
-     * @param node
-     */
-    void nodeNoLongerAvailable(TaskNode node) {
-        availableNodes.remove(node);
-    }
-
-    /**
-     * Returns all the available nodes in the graph
-     * @return
-     */
-    public List<TaskNode> getAvailableNodes() {
-        return availableNodes;
-    }
-    
 }
