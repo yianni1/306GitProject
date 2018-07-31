f6beebebb5b18df0ef64273a76f792f19bb2da90
yianni1 <32092681+yianni1@users.noreply.github.com>
2018-07-31 14:45:05 +1200

    Started implementation in solutiontree

--- 306Project/src/main/java/scheduling/SolutionTree.java
+++ 306Project/src/main/java/scheduling/SolutionTree.java
@@ -4,6 +4,7 @@ import graph.TaskEdge;
 import graph.TaskGraph;
 import graph.TaskNode;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -13,20 +14,58 @@ import java.util.Map;
  */
 public class SolutionTree {
 
-    private Map<Integer, Processor> processors = new HashMap<Integer, Processor>();
+    private List<Processor> processorList;
+    private TaskGraph graph;
 
     /**
      * Constructor builds a solution tree, with all the solutions
      * @param startingNodes
      * @param processorNumber
      */
-    public SolutionTree(List<TaskNode> startingNodes, int processorNumber) {
+    public SolutionTree(TaskGraph graph, int processors) {
 
-        //Creating the processors
-        for (int i = 0; i < processorNumber; i++) {
-            processors.put(i, new Processor(i));
+    	processorList = new ArrayList<Processor>();
+
+        for (int i = 0; i < processors; i++) {
+        	processorList.add(new Processor(i));
         }
-
-
+    	this.graph = graph;
+    	
     }
+    
+    public void doSchedule() {
+    	/*List<TaskNode> initialNodes = graph.getAvailableNodes();
+    	
+    	//Adds initial nodes with no incomming edges to processor
+    	int minimialInitialNodeweight = 0;
+    	TaskNode initialNode = initialNodes.get(0);
+    	
+    	for (TaskNode taskNode : initialNodes) {
+    		
+    		// Gets node with minimal weight and keeps it
+    		int weight = taskNode.getWeight();
+    		if (minimialInitialNodeweight < weight) { 
+    			minimialInitialNodeweight = weight;
+    			initialNode = taskNode;
+    		}
+    	}
+    
+    	//processorList.get(0).addTask(initialNode);
+    	
+    	TaskNode smallestTaskNode = null;
+    	
+    	// 
+    	for (TaskEdge edge : initialNode.getOutgoingEdges()) {
+    		
+    		TaskNode childNode = edge.getEndNode();
+    		int childweight = childNode.getWeight();
+    		
+    		if (smallestTaskNode.getWeight() < childweight) {
+    			smallestTaskNode = childNode;
+    		}
+    	}*/
+    	
+    }
+    
+
 }
