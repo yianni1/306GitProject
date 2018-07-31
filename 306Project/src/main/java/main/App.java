c009d151140cbc5e48d48e75da069ea5bb86021e
yianni1 <32092681+yianni1@users.noreply.github.com>
2018-07-31 14:40:33 +1200

    Added scheduling method to app

--- 306Project/src/main/java/main/App.java
+++ 306Project/src/main/java/main/App.java
@@ -14,6 +14,7 @@ import org.apache.commons.cli.Options;
 
 import javafx.application.Application;
 import javafx.stage.Stage;
+import scheduling.SolutionTree;
 
 /**
  * Hello world!
@@ -73,7 +74,8 @@ public class App extends Application{
 
 			GraphLoader loader = new GraphLoader();
 			TaskGraph graph = loader.load(fileName);
-
+			SolutionTree solution = new SolutionTree(graph, processorNumber);
+			solution.doSchedule();
 			System.out.println("Done"); // FOR DEBUGGING ON CONSOLE
         }
 	}
