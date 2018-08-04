f581dc83180cd38ec96bd1e5d14b072b88db236a
dkap813 <dkap813@EN-368395.UoA.auckland.ac.nz>
2018-08-04 17:57:04 +1200

    Added GraphLoaderTests to AllTests
    added the graph loader tests into the AllTests

--- 306Project/src/test/java/testCases/AllTests.java
+++ 306Project/src/test/java/testCases/AllTests.java
@@ -9,7 +9,12 @@ import java.util.Arrays;
 import java.util.HashSet;
 import java.util.List;
 
+import org.graphstream.graph.Edge;
 import org.graphstream.graph.Graph;
+import org.graphstream.graph.Node;
+import org.graphstream.graph.implementations.SingleGraph;
+import org.graphstream.stream.file.FileSource;
+import org.graphstream.stream.file.FileSourceFactory;
 import org.junit.Before;
 import org.junit.Test;
 
@@ -28,6 +33,7 @@ public class AllTests extends testCases.CompareOutput {
 	//Test Variables
     private static List<String> filePaths;
 	private static int processorNum;
+	private static Graph graphStreamGraph;
 
 	@Before
 	public void initialisePaths() {
@@ -271,7 +277,7 @@ public class AllTests extends testCases.CompareOutput {
 	    public void testNodes7() throws IOException {
 
 	        // Put a unique file name (doesn't have to be the name of the graph), and the graphs file path.
-		 	GraphLoader loader = new GraphLoader();
+		 	/*GraphLoader loader = new GraphLoader();
 	        String outputFileName = "Nodes7";
 	        TaskGraph graph = loader.load("src/main/resources/DotFiles/Nodes_7_OutTree.dot");
 	        SchedulerI scheduler = new GreedyScheduler(graph, 2);
@@ -297,9 +303,113 @@ public class AllTests extends testCases.CompareOutput {
 	        Output.createOutput(processes, graph, outputFileName + "CorrectSolution");
 
 	        boolean same = compareTextFiles("src/main/resources/DotFiles/" + outputFileName +"TestSolution-output.dot", "src/main/resources/DotFiles/" + outputFileName + "CorrectSolution-output.dot");
-	        assertTrue(same);
+	        assertTrue(same);*/
 	    }
-	
+	 
+	 @Test
+		public void testGraphLoader() {
+			for (String filePath : filePaths) {
+				System.out.println(filePath);
+				loadGraphStreamGraph(filePath);
+				testGraphLoader(filePath);
+			}
+		}
+
+		/**
+		 * Tests graph loader for a particular graph
+		 * @param filePath
+		 */
+		private void testGraphLoader(String filePath) {
+			GraphLoader loader = new GraphLoader();
+			TaskGraph graph = loader.load(filePath);
+
+			//This block checks the node names and weights are the same
+			HashSet<TaskNode> nodes = graph.getNodes();
+			for (TaskNode taskNode : nodes) {
+				boolean nodesTrue = false;
+				boolean nodeWeight = false;
+
+				for (Node graphStreamNode : graphStreamGraph) {
+					//If GraphStream Graph node name equals taskNode name, node is correct
+					if (graphStreamNode.toString().equals(taskNode.getName())) {
+						nodesTrue = true;
+					}
+
+					//If GraphStream weight is equal to TaskNode weight, nodeWeight is correct.
+					double nod =  Double.parseDouble(graphStreamNode.getAttribute("Weight").toString());
+					int nodeWeightInt = (int) nod;
+					if (taskNode.getWeight() == nodeWeightInt) {
+						nodeWeight = true;
+					}
+				}
+				//node names and weights should be true for all iterations
+				assertTrue(nodesTrue);
+				assertTrue(nodeWeight);
+			}
+
+			HashSet<TaskEdge> edges = graph.getEdges();
+
+			//This block checks the edges source and target nodes match the GraphStream graph
+			//Also checks weights
+			for (TaskEdge taskEdge : edges) {
+				boolean sourceNodeFromEdges = false;
+				boolean targetNodeFromEdges = false;
+				boolean taskEdgeWeight = false;
+
+				for (Edge edge : graphStreamGraph.getEdgeSet()) {
+					Node source = edge.getSourceNode();
+					Node target = edge.getTargetNode();
+
+					//If sourceNode name of GraphStream edge equals name of taskEdge start node,
+					//TaskGraph source node of TaskEdge is correct
+					if (source.toString().equals(taskEdge.getStartNode().getName())) {
+						sourceNodeFromEdges = true;
+					}
+
+					//If targetNode name of GraphStream edge equals name of taskEdge end node,
+					//TaskGraph target node of TaskEdge is correct
+					if (target.toString().equals(taskEdge.getEndNode().getName())) {
+						targetNodeFromEdges = true;
+					}
+
+					//Ensures weights are the same for TaskEdge and edge
+					double edgeWeight = Double.parseDouble(edge.getAttribute("Weight").toString());
+					int edgeWeightInt = (int) edgeWeight;
+					if (edgeWeightInt == taskEdge.getWeight()) {
+						taskEdgeWeight = true;
+					}
+
+				}
+				//edge names and weights should be true for all iterations
+				assertTrue(sourceNodeFromEdges);
+				assertTrue(targetNodeFromEdges);
+				assertTrue(taskEdgeWeight);
+
+			}
+		}
+
+
+		private void loadGraphStreamGraph(String filePath) {
+			Graph graph = new SingleGraph("graph"); // Creates graph
+			FileSource fs = null;
+
+			// Loads graph from filepath
+			try {
+				fs = FileSourceFactory.sourceFor(filePath);
+
+				fs.addSink(graph);
+
+				fs.readAll(filePath);
+
+			} catch( IOException e) {
+
+			} finally {
+				fs.removeSink(graph);
+			}
+			graphStreamGraph = graph;
+		}
+			
+			
 
 
 }
