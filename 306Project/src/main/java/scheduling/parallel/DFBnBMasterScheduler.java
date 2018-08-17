9312964d93998eeae7623156bf577ed8cc4196b5
Cyb3rX3r0 <37169775+Cyb3rX3r0@users.noreply.github.com>
2018-08-18 01:06:03 +1200

    Tidied up Master Scheduler, added comments

--- 306Project/src/main/java/scheduling/parallel/DFBnBMasterScheduler.java
+++ 306Project/src/main/java/scheduling/parallel/DFBnBMasterScheduler.java
@@ -28,8 +28,10 @@ import java.util.concurrent.TimeUnit;
  */
 public class DFBnBMasterScheduler {
 
+	//variables around visualization
 	private VisualisationController scheduleListener;
 
+	//vars around the algorithim
 	private List<Schedule> partialSchedules = new ArrayList<>();
 	private int numCores;
 
@@ -39,16 +41,25 @@ public class DFBnBMasterScheduler {
 	private long numPaths;
 	private long branchesPruned;
 
-	// Index of the children of the schedule.
+
+	//Indexing for the children of the schedule
 	private List<Integer> nodeIndices;
 	private List<Integer> processorIndices;
 
+	//vars for traversal and storing schedules
 	private Schedule optimalSchedule;
 	private Schedule schedule;
 	private List<TaskNode> schedulableNodes;
 
 	private List<TaskNode> initialNodes;
 
+	/**
+	 * Constructor for the Master scheduler sets upperbound limit, and creates new schedule based on the graph
+	 * and the processor number
+	 * @param graph
+	 * @param processors
+	 * @param numCores
+	 */
 	public DFBnBMasterScheduler(TaskGraph graph, int processors, int numCores) {
 		upperBound = Integer.MAX_VALUE;
 		this.numCores = numCores;
@@ -56,12 +67,19 @@ public class DFBnBMasterScheduler {
 	}
 
 	/**
-	 * Runs a bfs to allocate a thread for each schedule
+	 * This method runs the Breadth first search iteratively on corresponding layer after the initai layer to assign
+	 * threads to each of the schedules
 	 */
 	private void runBFS() {
-		if (numCores<= schedule.getSchedulableNodes().size()) {
-			// Assign
+
+		//if the amount of cores is less then the amount of schedulable nodes
+		if (numCores <= schedule.getSchedulableNodes().size()) {
+
+			// Assign A thread to that particular node
+
 		} else {
+
+			//resetting values??
 			int numOfChildren=0;
 			int depthBFS=0;
 			int nodeIndex;
@@ -90,12 +108,18 @@ public class DFBnBMasterScheduler {
 		}
 	}
 
+	/**
+	 * Gets a list of partial schedules
+	 * @return partialSchedules
+	 */
 	public List<Schedule> getPartialSchedules() {
 		return partialSchedules;
 	}
 
 	/**
-	 * This method makes a "deep clone" of any object it is given.
+	 * This method makes a Deepclone of the object that is parsed into it
+	 * @param object
+	 * @return ois.readObject() //clone of the object
 	 */
 	private static Object deepClone(Object object) {
 		try {
@@ -112,20 +136,27 @@ public class DFBnBMasterScheduler {
 		}
 	}
 
-
+	/**
+	 * This method initializes the partial schedule which creates partial schedules if the
+	 * amount of core is less
+	 */
 	public void initialisePartialSchedules() {
+
 
 		int scheduleIndex = 0;
 
+		//if the number of partial schedules is less than that of the number of cores
 		while (partialSchedules.size() < numCores) {
 
 			try {
+				//creates a partial schedule
 				createPartialSchedules(schedule);
 			}
 			catch (CloneNotSupportedException e) {
 				e.printStackTrace();
 			}
 
+			//gets the current index of the partial schedules and then iterates to the next index
 			schedule = partialSchedules.get(scheduleIndex);
 			scheduleIndex++;
 
@@ -134,23 +165,35 @@ public class DFBnBMasterScheduler {
 	}
 
 
-
+	/**
+	 * This method creates partial schedules through deepclonning schedules which can then be checked for task addition
+	 * @param s
+	 * @throws CloneNotSupportedException
+	 */
 	private void createPartialSchedules(Schedule s) throws CloneNotSupportedException {
+
+		//iterating through the schedulable nodes
 		for (TaskNode node : s.getSchedulableNodes()) {
+
+			//iterating through processors
 			for(Processor p: s.getProcessors()) {
 
+				//creating a deepclone of schedule parameter
 				Schedule partialSchedule = (Schedule) deepClone(s);
 
-				//TODO nodes being schedule on multiple processes will already be scheduled on second iteration
-				// Of this loop. Means getEarliestSchedulableTime will be -1 and will cause not scheduled exception
+				//TODO make sure that nodes being scheduled on multiple processors will already be scheduled on second iteration
+
+				//the earliest schedulable time will be -1 when getting the value from this loop and this will not cause an scheduled exception
 				int time = partialSchedule.getEarliestSchedulableTime(node, p);
 
+				//add the task to the partial schedule and then to the list of partial schedules
 				partialSchedule.addTask(node, p, time);
 				partialSchedules.add(partialSchedule);
 			}
 
 		}
 
+		//if the partial contains s then it removes that s from the partial schedule list
 		if (partialSchedules.contains(s)) {
 			partialSchedules.remove(s);
 		}
