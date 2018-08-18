7ea11a24da6d39f91713da5da8aafc77c77927e7
nolatos <oliver.li47@gmail.com>
2018-08-18 18:23:31 +1200

    Commented out test cases

--- 306Project/src/main/java/scheduling/Processor.java
+++ 306Project/src/main/java/scheduling/Processor.java
@@ -64,17 +64,16 @@ public class Processor implements Serializable {
      * @return
      */
     public int getBound() {
-//        int bound = 0;
-//        for (TaskNode node : tasks) {
-//            if (node.getEndTime() > bound) {
-//                bound = node.getEndTime();
-//            }
-//        }
-//
-//        return bound;
+        int bound = 0;
 
-        //Returns the end time of the last task added
-        return tasks.get(tasks.size() - 1).getEndTime();  //Faster than looping through
+        //Looping through all of the nodes to get the lowest bound
+        for (TaskNode node : tasks) {
+            if (node.getEndTime() > bound) {
+                bound = node.getEndTime();
+            }
+        }
+
+        return bound;
     }
 
     /**
