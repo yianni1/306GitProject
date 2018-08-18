e1ac4d63bbe7b56e6c65b79dc9e1661306ee6a92
Cyb3rX3r0 <37169775+Cyb3rX3r0@users.noreply.github.com>
2018-08-18 20:07:20 +1200

    Claned and tidied up DFVNVMAsterSchedukar

--- 306Project/src/main/java/scheduling/DFBnBMasterScheduler.java
+++ 306Project/src/main/java/scheduling/DFBnBMasterScheduler.java
@@ -18,29 +18,38 @@ import static scheduling.Utilities.deepClone;
  */
 public class DFBnBMasterScheduler implements  Scheduler {
 
+	//variables for visualisation
 	private VisualisationController scheduleListener;
 
+	//variables associated with the partial schedules and normal schedules
 	private List<Schedule> partialSchedules = new ArrayList<>();
 	private List<DFBnBSlaveScheduler> slaves = new ArrayList<>();
+	private Schedule schedule;
+
+	//variables associated with processors
 	private int numCores;
 	private int processors;
+
+	//variables asssociated with graphs
 	private TaskGraph graph;
 	private int upperBound;
 	private long numPaths;
 	private long branchesPruned;
-	private Schedule schedule;
 
 	/**
-	 * Constructor which generates an initial empty schedule based on the processors and the graph
-	 *
+	 * Constructor which generates an initial empty schedule based on the processors , the graph and the number of cores
+	 * it contains
 	 * @param graph
 	 * @param processors
 	 * @param numCores
 	 */
 	public DFBnBMasterScheduler(TaskGraph graph, int processors, int numCores) {
+
 		this.graph = graph;
 		this.processors = processors;
 		this.numCores = numCores;
+
+		//creating new schedule with the number of processors and the Graph
 		schedule = new Schedule(processors, graph);
 		numPaths = 0;
 		branchesPruned = 0;
@@ -56,7 +65,12 @@ public class DFBnBMasterScheduler implements  Scheduler {
 
 
 		Schedule greedySchedule = new GreedyScheduler(graph, processors).createSchedule();
-		Schedule defaultGreedy = (Schedule) deepClone(greedySchedule); //Default greedy schedule to use if no better schedule found in slave
+
+		//Default Greedy schedule to be used in the slave if no better schedule can be found which results in the pruning
+		//of the branch
+		Schedule defaultGreedy = (Schedule) deepClone(greedySchedule);
+
+		// upadate the schedule with the greedy schedule
 		updateSchedule(defaultGreedy);
 
 		//intializing the upperbound
@@ -72,19 +86,28 @@ public class DFBnBMasterScheduler implements  Scheduler {
 		//Thread pool assigned with the number of cores
 		ExecutorService executor = Executors.newFixedThreadPool(numCores);
 
+		//this generates a list of all the optimal schedules which are found through the iterative search
         List<Schedule> locallyOptimalSchedules = new ArrayList<>();
 
 		//generating the number of threads associative to the number of cores
 		for (Schedule schedule : partialSchedules) {
 			final DFBnBMasterScheduler master = this;
+
+			//running on the executor thread
 			executor.execute(new Runnable() {
 				@Override
 				public void run() {
+
+					//generate a schedular to be inputted into the slave
 					DFBnBSlaveScheduler scheduler = new DFBnBSlaveScheduler(schedule.getGraph(), processors, schedule, upperBound, master);
+
+					//add the scheduler to the slave
 					slaves.add(scheduler);
+
+					//creates a schedule based on the schedular
 					Schedule s = scheduler.createSchedule();
 
-					//If bound found by thread is null, use default greedy
+					//If bound found by thread is null through pruning then use default greedy
 					if (s == null) {
 						locallyOptimalSchedules.add(defaultGreedy);
 					} else {
@@ -96,6 +119,8 @@ public class DFBnBMasterScheduler implements  Scheduler {
 		}
 
 		executor.shutdown();
+
+		//the thread then awaits the termination
 		try {
 			executor.awaitTermination(Long.MAX_VALUE, TimeUnit.NANOSECONDS);
 		} catch (InterruptedException e) {
@@ -125,25 +150,42 @@ public class DFBnBMasterScheduler implements  Scheduler {
 
 	}
 
+	/**
+	 * Sets the ScheduleListener for the GUI visulisation
+	 * @param scheduleListener which listens for changes that occur in the alglorithim
+	 */
 	public void setScheduleListener(VisualisationController scheduleListener) {
 		this.scheduleListener = scheduleListener;
 	}
 
 
 	/**
-	 * Updates GUI with new optimal schedule and also notifies slaves of a better bound if necessary.
-	 *
+	 * The GUI is updated with a new optimal schedule every time it finds a better solution and it notifies the slaves
+	 * of a better bound if it is necessary
 	 * @param schedule
 	 */
 	public void updateSchedule(Schedule schedule) {
+
+		//checking there is schedule listener present
 		if (this.scheduleListener != null) {
+
+			//checking the current bound is lower than the upperbound
 			if (schedule.getBound() < upperBound) {
+
+				//changes the lowerbound to that of the upperbound
 				upperBound = schedule.getBound();
+
+				//notifies the slaves of the updated Upperbound while they perform DFS
 				for (DFBnBSlaveScheduler slave : slaves) {
 					slave.updateUpperBound(upperBound);
 				}
+
+				//updates the schedule in the GUI
 				scheduleListener.updateSchedule(schedule);
+
+
 				try {
+					//makes the thread sleep for 200msecs
 					TimeUnit.MILLISECONDS.sleep(200);
 				} catch (InterruptedException e) {
 					e.printStackTrace();
@@ -152,28 +194,53 @@ public class DFBnBMasterScheduler implements  Scheduler {
 		}
 	}
 
+	/**
+	 * This method updates the amount of branches pruned in the GUI
+	 * @param branchesPruned (The number of branhces pruned)
+	 */
 	public void updateBranchesPruned(long branchesPruned) {
+
+		//adds the new amount of branches pruned to that of the previous value
 		this.branchesPruned = this.branchesPruned + branchesPruned;
+
+		//checking for there is a schedule listener object actually present
 		if (this.scheduleListener != null) {
 			scheduleListener.updateBranchesPruned(this.branchesPruned);
 		}
 	}
 
+	/**
+	 * Updates the numOfPaths Statistic in the GUI
+	 * @param numPaths , statistic for the number of paths
+	 */
 	public void updateNumPaths(long numPaths) {
+
+		//adds on the to the previous value of numofpaths
 		this.numPaths = this.numPaths + numPaths;
+
+		//check if the ScheduleListener object is present
 		if (this.scheduleListener != null) {
 			scheduleListener.updateNumPaths(this.numPaths);
 		}
 
 	}
 
+	/**
+	 * This method finishes the listening for the events of the schedulelistener
+	 */
 	public void finish() {
+
+		//checking if hte schedule listener object is present
 		if (this.scheduleListener != null) {
 			for (DFBnBSlaveScheduler slave : slaves) {
+
+				//check if the slave threads have completed there DFS iteration
 				if (!slave.isFinished()) {
 					return;
 				}
 			}
+
+			//finished the schedulelistener object
 			scheduleListener.finish();
 		}
 	}
@@ -213,15 +280,20 @@ public class DFBnBMasterScheduler implements  Scheduler {
 		}
 
 		List<Schedule> removeSchedule = new ArrayList<>();
+
 		//Code to remove duplicated initial nodes on partial schedules
 		for (int i = 0; i < partialSchedules.size(); i++) {
 			for (int j = i; j < partialSchedules.size(); j++)
 
+				//checks if the partial schedules are the same
 				if (equals(partialSchedules.get(i), partialSchedules.get(j))) {
+
+					//adds that partial schedule to seperate schedule
 					removeSchedule.add(partialSchedules.get(i));
 				}
 		}
 
+		//removes the schedule from the partial schedule list
 		for(Schedule s :removeSchedule) {
 			partialSchedules.remove(s);
 		}
@@ -254,7 +326,7 @@ public class DFBnBMasterScheduler implements  Scheduler {
 	}
 
 	/**
-	 * Equals method to compare schedules
+	 * This method comapress the schedules to each other
 	 * @param s1
 	 * @param s2
 	 * @return
@@ -262,12 +334,19 @@ public class DFBnBMasterScheduler implements  Scheduler {
 	private boolean equals(Schedule s1, Schedule s2) {
 
 		boolean same = false;
+
+		//get the amount of processors for both of them
 		for (Processor p : s1.getProcessors()) {
 			for (Processor c : s2.getProcessors()) {
+
+				//get the Id of both both the procceors and checks if the processors are not equal
 				if (p.getID() != c.getID()) {
 
+					//get the tasks that have been associated with each of the nodes
 					for (TaskNode task : p.getTasks()) {
 						for (TaskNode otherTask : c.getTasks()) {
+
+							//check if names of the tasks and then the earleist startt
 							if ((task.getName().equals(otherTask.getName())) && (task.getStartTime() == otherTask.getStartTime()) ) {
 								same = true;
 							}
